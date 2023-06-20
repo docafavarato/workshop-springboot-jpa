f47f19675a6f99f247a159dbef7e3c06463a64a6
São Francisco Vet <docafavarato@gmail.com>
2023-06-19 21:05:23 -0300

    first commit

---
+++ src/test/java/com/favarato/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favarato.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
