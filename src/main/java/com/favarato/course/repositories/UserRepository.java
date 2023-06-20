4dc8ca1aa1e792b1f605a79631c33f5bd78ffc15
São Francisco Vet <docafavarato@gmail.com>
2023-06-20 15:06:50 -0300

    Service layer, component registration

--- src/main/java/com/favarato/course/repositories/UserRepository.java
+++ src/main/java/com/favarato/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.favarato.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.favarato.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
