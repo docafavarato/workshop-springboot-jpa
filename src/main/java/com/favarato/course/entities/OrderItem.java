747cf91e69b6e5d0cbdf63bb4c65eea661d809f7
São Francisco Vet <docafavarato@gmail.com>
2023-06-26 20:27:29 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/favarato/course/entities/OrderItem.java
+++ src/main/java/com/favarato/course/entities/OrderItem.java
@@ -3,17 +3,30 @@ package com.favarato.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.favarato.course.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
 	private Integer quantity;
 	private Double price;
 	
 	public OrderItem() {
 	}
-	
-	public OrderItem(Integer quantity, Double price) {
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
+		id.setOrder(order);
+		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
@@ -33,14 +46,27 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
-	public Double subTotal() {
-		return this.getPrice() * this.getQuantity();
+	
+	@JsonIgnore
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(price, quantity);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -52,6 +78,6 @@ public class OrderItem implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		OrderItem other = (OrderItem) obj;
-		return Objects.equals(price, other.price) && Objects.equals(quantity, other.quantity);
+		return Objects.equals(id, other.id);
 	}
 }
---
+++ src/main/java/com/favarato/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.favarato.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.favarato.course.entities.OrderItem;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+}
